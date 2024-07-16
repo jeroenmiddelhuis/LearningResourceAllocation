6ebd772c9a3e8f2693671557cd2229b8d7bfd002
eliransc <scherzter@gmail.com>
2024-07-16 21:05:22 +0300

    First commit eliran revision

---
+++ sbatch_cedar_bayesopt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -t 0-23:58
+#SBATCH -A def-dkrass
+#SBATCH --mem 10000
+source /home/eliransc/projects/def-dkrass/eliransc/queues1/queues/bin/activate
+python /home/eliransc/projects/def-dkrass/eliransc/LearningResourceAllocation/bayesopt.py
