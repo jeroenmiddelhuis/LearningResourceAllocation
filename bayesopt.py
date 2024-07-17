0811c838c0966b9209303f24b086235d30af84b7
eliransc <scherzter@gmail.com>
2024-07-17 14:41:37 +0300

    First commit eliran revision

--- bayesopt.py
+++ bayesopt.py
@@ -38,13 +38,17 @@ import pandas as pd
 #'complete_parallel', 'complete_reversed', 'complete',
 
 
+
+
+
 listis = [ 'parallel', 'n_system', 'down_stream', 'slow_server', 'high_utilization', 'low_utilization']
 configtype = np.random.choice(listis)
 # configtype = 'low_utilization'
+# configtype = 'complete_reversed'
 
 arrival_list = [0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6]
 arrival_rate = np.random.choice(listis)
-arrival_rate = 0.5
+# arrival_rate = 0.5
 
 print(arrival_rate, configtype)
 # You can build your bayesian optimization model around this framework:
@@ -100,7 +104,7 @@ def aggregate_sims1(A):
         tot_res.append(res)
 
     print(np.array(tot_res).mean())
-    pkl.dump((tot_res,A), open('./eliran_results/'+configtype + '_arrival_rate_' +str(arrival_rate)+'_model_num_' + str(model_num) + '.pkl', 'wb'))
+    # pkl.dump((tot_res,A), open('./eliran_results/'+configtype + '_arrival_rate_' +str(arrival_rate)+'_model_num_' + str(model_num) + '.pkl', 'wb'))
     return np.array(tot_res).mean()  #tot_res #
 
 
@@ -114,27 +118,46 @@ def main():
 
     print(arrival_rate)
 
+
+
+
     if configtype == 'low_utilization':
-        A_vals = [1.209298, 4.898732, 0.496618, 2.658753, 2.234656, 12.339795, 2000]
+        A_vals = [1.209298, 4.898732, 0.496618, 2.658753, 2.234656, 12.339795, 200000]
     elif configtype == 'high_utilization':
-        A_vals =  [1.321091, 4.556206,  0.790609,0.000000, 3.051695,    15.739693, 2000]
+        A_vals =  [1.321091, 4.556206,  0.790609,0.000000, 3.051695,    15.739693, 200000]
     elif configtype == 'down_stream':
-        A_vals = [1.655077, 4.013198,0.491260, 0.279967, 2.213313, 7.287871, 20000]
+        A_vals = [1.655077, 4.013198,0.491260, 0.279967, 2.213313, 7.287871, 2000000]
     elif configtype == 'n_system':
-        A_vals = [0.000000,	5.000000,	1.584558,	0.000000,	0.000000	, 7.0, 2000]
+        A_vals = [0.000000,	5.000000,	1.584558,	0.000000,	0.000000	, 7.0, 200000]
     elif configtype == 'slow':
-        A_vals = [5.000000, 3.767883, 0.0, 1.645102, 0.000000, 20.000000, 20000]
+        A_vals = [5.000000, 3.767883, 0.0, 1.645102, 0.000000, 20.000000, 2000000]
     elif configtype == 'complete_all':
-        A_vals= [0.337376 ,   4.110818,    1.167821,    0.862430 ,   0.474204 ,   19.111311, 20000]
+        A_vals= [0.337376 ,   4.110818,    1.167821,    0.862430 ,   0.474204 ,   19.111311, 20000000]
+
+    elif configtype == 'parallel':
+        A_vals=  [0.15, 0.15, 20.0, 20.0, 20.0, 0.15, 8.28753107297809]
+
+    elif configtype == 'complete_reversed':
+        A_vals=  [0.015,  0.015,     25.0,    0.015,   0.015,     0.015,    5.0]
+
+    elif configtype == 'complete':
+        A_vals= [15.42641286533492, 0.41503898718803, 12.672964698525508, 14.976077650772236, 9.970140246051809,
+                4.495932910616953, 59.9031432379812]
+
+    elif configtype == 'complete_parallel':
+        A_vals= [16.25241923304227, 12.250521336587763, 14.43510634863599, 5.837521363412663, 18.35548245025887,
+                         14.291515667953812, 377.12721840056307]
 
 
-    space = [Real(A_vals[0]-0.5, A_vals[0]+0.5, name='a1'),
-             Real(A_vals[1]-0.5, A_vals[1]+0.5, name='a2'),
-             Real(A_vals[2]-0.5, A_vals[2]+0.5, name='a3'),
-             Real(A_vals[3]-0.5, A_vals[3]+0.5, name='a4'),
-             Real(A_vals[4]-0.5, A_vals[4]+0.5, name='a5'),
-             Real(A_vals[5]-0.5, A_vals[5]+0.5, name='a6'),
-             Real(A_vals[6]-0.5, A_vals[6]+0.5, name='a7')]
+    space = [Real(max(0,A_vals[0]-0.5), A_vals[0]+1, name='a1'),
+             Real(max(0,A_vals[1]-0.5), A_vals[1]+1, name='a2'),
+             Real(max(0,A_vals[2]-0.5), A_vals[2]+1, name='a3'),
+             Real(max(0,A_vals[3]-0.5), A_vals[3]+1.5, name='a4'),
+             Real(max(0,A_vals[4]-0.5), A_vals[4]+1.5, name='a5'),
+             Real(max(0,A_vals[5]-0.5), A_vals[5]+1.5, name='a6'),
+             Real(max(0,A_vals[6]-0.5), A_vals[6]+1.5, name='a7')]
+
+
 
     res = gp_minimize(aggregate_sims1,  # the function to minimize
                       space,  # the bounds on each dimension of x
@@ -147,7 +170,7 @@ def main():
 
     model_num = np.random.randint(0, 100000)
 
-    pkl.dump((res.func_vals, res.x_iters), open('./eliran_results/' + str(model_num) + 'arrival_rate_' +str(str(arrival_rate)) +'_low_utilization.pkl', 'wb'))
+    pkl.dump((res.func_vals, res.x_iters), open('./eliran_results/' + str(model_num) + 'arrival_rate_' +str(str(arrival_rate)) +'_config_'+configtype+ '.pkl', 'wb'))
 
 
 
