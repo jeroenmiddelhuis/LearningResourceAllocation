caf6267f7d0694436e141f564ea85cebc093a2d1
eliransc <scherzter@gmail.com>
2024-07-21 10:18:12 +0300

    First commit eliran revision

--- sbatch_train_SVFA.sh
+++ sbatch_train_SVFA.sh
@@ -3,4 +3,4 @@
 #SBATCH -A def-dkrass
 #SBATCH --mem 10000
 source /home/eliransc/projects/def-dkrass/eliransc/queues1/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/LearningResourceAllocation/bayesopt.py
+python /home/eliransc/projects/def-dkrass/eliransc/LearningResourceAllocation/train_SVFA.py
